107c3a9ad3b1242aeea3252aa0adb6e6bb77ae07
vcastellar <vic.sebastia@gmail.com>
2021-02-07 18:20:30 +0100

    imputación de obnservaciones ausentes popr interpolación

--- generarPlotsCV.R
+++ generarPlotsCV.R
@@ -13,14 +13,14 @@ datos <- read.csv("datos.csv")
 
 # datos por grupo de edad
 datosEdad <- datos %>% group_by(fecha, GrupoEdad) %>% 
-  summarise(CasosAcum = sum(CasosAcum),
-            CasosDia  = sum(CasosDia),
-            DefAcum   = sum(DefAcum),
-            DefDia    = sum(DefDia),
-            Casos_7d  = sum(Casos_7d),
-            Casos_14d = sum(Casos_14d),
-            Def_7d    = sum(Def_7d),
-            Def_14d   = sum(Def_14d)
+  summarise(CasosAcum = sum(CasosAcum, na.rm = TRUE),
+            CasosDia  = sum(CasosDia, na.rm = TRUE),
+            DefAcum   = sum(DefAcum, na.rm = TRUE),
+            DefDia    = sum(DefDia, na.rm = TRUE),
+            Casos_7d  = sum(Casos_7d, na.rm = TRUE),
+            Casos_14d = sum(Casos_14d, na.rm = TRUE),
+            Def_7d    = sum(Def_7d, na.rm = TRUE),
+            Def_14d   = sum(Def_14d, na.rm = TRUE)
             )
 
 p <- ggplot(datosEdad, aes(as.Date(fecha), CasosDia, color = GrupoEdad)) + geom_bar(stat = "identity") +
@@ -53,4 +53,36 @@ p <- ggplot(datosEdad, aes(as.Date(fecha), DefDia, color = GrupoEdad)) + geom_ba
 png(filename = "./plots/defuncionesXGruposEdad.png", width = 1800, height = 900)
 p
 dev.off()
+
+#------------------------------------------------------------------------------
+# datos de incidencia y defunciones totales
+#------------------------------------------------------------------------------
+datosTotales <- datos %>% group_by(fecha) %>% 
+  summarise(CasosAcum = sum(CasosAcum),
+            CasosDia  = sum(CasosDia),
+            DefAcum   = sum(DefAcum),
+            DefDia    = sum(DefDia),
+            Casos_7d  = sum(Casos_7d),
+            Casos_14d = sum(Casos_14d),
+            Def_7d    = sum(Def_7d),
+            Def_14d   = sum(Def_14d)
+  )
+
+# grafico de casos
+p <- ggplot(datosTotales, aes(as.Date(fecha), CasosDia)) + 
+  geom_line(stat = "identity",
+           color = palette$green) +
+  labs(title = "nº casos diarios en CV",
+       subtitle = paste("fecha de generación: ", Sys.Date()),
+       caption = "Fuente de Datos: https://dadesobertes.gva.es") +
+  xlab("Fecha") + ylab("Casos Diarios") +
+  scale_x_date(date_breaks = "1 month") +
+  custom_theme + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                       legend.position = "none",
+                       panel.grid.major.x = element_blank())
+png(filename = "./plots/casosDiarios.png", width = 1800, height = 900)
+p
+dev.off()
+  
+
 
