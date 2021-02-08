68716319e9669ef842a3645b0cd82e3a755c2a68
vcastellar <vic.sebastia@gmail.com>
2021-02-08 06:12:37 +0100

    se agrega plot de resumen de la situación

--- generarPlotsCV.R
+++ generarPlotsCV.R
@@ -2,6 +2,7 @@
 
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
 library(plotly)
 
 source("customTheme.R")
@@ -69,7 +70,7 @@ datosTotales <- datos %>% group_by(fecha) %>%
   )
 
 # grafico de casos
-p <- ggplot(datosTotales, aes(as.Date(fecha), CasosDia)) + 
+p1 <- ggplot(datosTotales, aes(as.Date(fecha), CasosDia)) + 
   geom_bar(stat = "identity",
            color = palette$green) +
   labs(title = "nº casos diarios en CV",
@@ -80,9 +81,84 @@ p <- ggplot(datosTotales, aes(as.Date(fecha), CasosDia)) +
   custom_theme + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
                        legend.position = "none",
                        panel.grid.major.x = element_blank())
-png(filename = "./plots/casosDiarios.png", width = 1800, height = 900)
-p
+
+# grafico de defunciones
+p2 <- ggplot(datosTotales, aes(as.Date(fecha), DefDia)) + 
+  geom_bar(stat = "identity",
+           color = palette$red) +
+  labs(title = "nº de defunciones diarias en CV",
+       subtitle = paste("fecha de generación: ", Sys.Date()),
+       caption = "Fuente de Datos: https://dadesobertes.gva.es") +
+  xlab("Fecha") + ylab("Defunciones Diarias") +
+  scale_x_date(date_breaks = "1 month") +
+  custom_theme + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                       legend.position = "none",
+                       panel.grid.major.x = element_blank())
+
+
+# gráfico de incidencia 14 días
+p3 <- ggplot(datosTotales, aes(as.Date(fecha), Casos_14d / 5003769 * 1e5)) + 
+  geom_bar(stat = "identity",
+           color = palette$lightorange) +
+  labs(title = "tasa 14 días por cada 100.000 habitantes",
+       subtitle = paste("fecha de generación: ", Sys.Date()),
+       caption = "Fuente de Datos: https://dadesobertes.gva.es") +
+  xlab("Fecha") + ylab("nº casos x 100.000 habitantes") +
+  scale_x_date(date_breaks = "1 month") +
+  custom_theme + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                       legend.position = "none",
+                       panel.grid.major.x = element_blank())
+
+
+# gráfico razón de tasas
+p4 <- ggplot(datosTotales, aes(as.Date(fecha), c(rep(NA, 7), diff(log(Casos_14d), lag = 7) + 1))) + 
+  geom_line(stat = "identity",
+           color = palette$gold) +
+  labs(title = "razón de tasas 14 días por cada 100.000 habitantes",
+       subtitle = paste("fecha de generación: ", Sys.Date()),
+       caption = "Fuente de Datos: https://dadesobertes.gva.es") +
+  xlab("Fecha") + ylab("razón de tasas") +
+  scale_x_date(date_breaks = "1 month") +
+  geom_smooth(span = 0.25, color = palette$skyblue) +
+  custom_theme + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                       legend.position = "none",
+                       panel.grid.major.x = element_blank())
+
+
+# gráfico índice de fatalidad
+p5 <- ggplot(datosTotales, aes(as.Date(fecha), DefAcum / CasosAcum * 1e2)) + 
+  geom_smooth(span = 10,
+              color = palette$red,
+              level = 0.95) +
+  labs(title = "evolución del índice de fatalidad",
+       subtitle = paste("fecha de generación: ", Sys.Date()),
+       caption = "Fuente de Datos: https://dadesobertes.gva.es") +
+  xlab("Fecha") + ylab("índice de fatalidad") +
+  scale_x_date(date_breaks = "1 month") +
+  custom_theme + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                       legend.position = "none",
+                       panel.grid.major.x = element_blank())
+
+# gráfico casos acumulados
+p6 <- ggplot(datosTotales, aes(as.Date(fecha), CasosAcum)) + 
+  geom_line(stat = "identity",
+              color = palette$green,
+              size = 1.25) +
+  labs(title = "evolución del número de casos acumulados",
+       subtitle = paste("fecha de generación: ", Sys.Date()),
+       caption = "Fuente de Datos: https://dadesobertes.gva.es") +
+  xlab("Fecha") + ylab("número de casos") +
+  scale_x_date(date_breaks = "1 month") +
+  custom_theme + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                       legend.position = "none",
+                       panel.grid.major.x = element_blank())
+
+
+png(filename = "./plots/resumenSituacion.png", width = 1800, height = 900)
+grid.arrange(p1, p2, p3, p6, p5, p4, ncol = 3, nrow = 2)
 dev.off()
-  
+
+
+
 
 
